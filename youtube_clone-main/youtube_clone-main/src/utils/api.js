24e8df2eb467dbb16ae0c917d72417abea5189a7
Shyamashree das <137187463+shyamaS7shree@users.noreply.github.com>
2024-08-22 12:44:41 +0530

    Add files via upload

---
+++ youtube_clone-main/youtube_clone-main/src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "b9d0b4ebcdmsh49b0647f48d808ap1ab2a9jsna066e0721300",  
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
